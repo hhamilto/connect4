af509d0371cd603a4784bff04467d1d64aab4dd7
Hurricane Hamilton <hhamilto@mtu.edu>
2014-10-09 22:18:29 -0400

    AFTER USING THOSE STUPID BIT FIELDS AND MEMOIZATION IN THE JS PYTHON STILL OUT PERFORMED IT BY ~1.5 TIMES. man. perahps its just the cleaner code that I wrote second time through.

--- connect.py
+++ connect.py
@@ -4,10 +4,10 @@ import sys
 RED = 1;
 BLACK = 2;
 PLAYABLE = 0;
-BOARDWIDTH=6;
+BOARDWIDTH=7;
 BOARDHEIGHT=6;
 K=4;
-S_DEPTH=2;
+S_DEPTH=5;
 
 #boards are index by rows going 0 to right most then columns from 0 to top
 def newBoard():
@@ -94,7 +94,7 @@ def printBoard(board):
 		sys.stdout.write('\n')
 
 
-
+# so this thing guestimates how "good" a board is
 def heuristic(board):
 	score = 0
 	#compute red's  "goodness" or advantage
@@ -111,20 +111,84 @@ def heuristic(board):
 	for win in allWins:
 		filled = 0;
 		for cell in win:
-			if board[cell[0]][cell[1]] == RED:
+			if board[cell[0]][cell[1]] == BLACK:
 				filled = filled+1
-			elif board[cell[0]][cell[1]] == BLACK:
+			elif board[cell[0]][cell[1]] == RED:
 				filled = -K
 		if filled > 0 :
-			score = score + filled*filled*filled*filled / (K*1.0)#force floating point?
+			score = score - filled*filled*filled*filled / (K*1.0)#force floating point?
 	return score;
-}
+
+def other(color):
+	return RED if color==BLACK else BLACK
+
+def play(node,depth):
+	node["state"] = getState(node["board"]);
+
+	#print "hello"
+	if node["state"] != PLAYABLE :
+		node["favorability"] = 2147483647 if node["state"]==RED else -2147483647; #red win is good, black win is bad
+		return node;
+
+	if depth == 0:
+		node["favorability"] = heuristic(node["board"]);
+		return node;
+
+	#print "hello"
+	node["children"] = [];
+	for i in xrange(BOARDWIDTH):
+		if node["board"][i][BOARDHEIGHT-1] != 0: #slot is full
+			continue
+		nextBoard = copyBoard(node["board"])
+		dropIn(i,nextBoard,node["toMove"]);
+		#print "makin kids"
+		node["children"].append(play({ "board": nextBoard, "toMove": other(node["toMove"]) },depth-1));
+
+	#print "hello"
+
+	if node["toMove"] ==RED: #red picks the best move
+		maxVal = -2147483600
+		for child in node["children"]:
+			if child["favorability"] > maxVal:
+				maxVal = child["favorability"]
+		node["favorability"] = maxVal
+	elif node["toMove"] == BLACK: #black picks shittiest move for red
+		minVal = 2147483600
+		for child in node["children"]:
+			if child["favorability"] < minVal:
+				minVal = child["favorability"]
+		node["favorability"] = minVal
+	else:
+		print "fuck." #fucked up
+
+	return node;
 
 
 def main():
 	#print allWins
+	board = newBoard();
+	while True:
+		gameTree = play({ "board": board, "toMove": RED },S_DEPTH);
+		#computer picks best move
+		best = gameTree["children"][0]
+		for child in gameTree["children"]:
+			if child["favorability"] > best["favorability"]:
+				best = child
+		board = best["board"]
+		print "Computer moved:"
+		printBoard(board)
+		print "Enter column to drop your piece in: (1 indexed)"
+		column = 0
+		while column == 0:
+			try:
+				column = int(raw_input())
+			except ValueError:
+				print 'Invalid Number'
+				column = 0
+		dropIn(column-1, board, BLACK);
+		printBoard(board)
 
-	if True: #tests to make sure stuffs not fucked up
+	if False: #tests to make sure stuffs not fucked up
 		print getState([[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]) == 0
 		print getState([[1,1,1,1,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]) == 1
 		print getState([[1,1,1,2,0,0],[0,0,0,2,0,0],[0,0,0,2,0,0],[0,0,0,2,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]) == 2
@@ -143,5 +207,8 @@ def main():
 		dropIn(2,copyBoard(board),BLACK)
 		print board == [[1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [2, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
 
-		print heuristic([[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]])
+		print heuristic([[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]) == 0
+		print heuristic([[1,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]) > 0
+		print heuristic([[1,1,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]) > heuristic([[1,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]])
+		print heuristic([[1,1,2,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]) < heuristic([[1,1,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[2,0,0,0,0,0]])
 if __name__ == "__main__": main()
